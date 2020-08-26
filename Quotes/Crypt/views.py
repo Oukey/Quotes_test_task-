98d1b32b2f9a273ec8fa69488d5e17dbd1d41884
Oukey <oukey311@gmail.com>
2020-08-26 22:20:40 +0400

    Добавлен функционал QuotesNow

--- Quotes/Crypt/views.py
+++ Quotes/Crypt/views.py
@@ -1,6 +1,8 @@
-import requests, datetime
+import requests
 
 from rest_framework import generics, permissions, serializers
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import CryptQuotes
 
@@ -15,3 +17,11 @@ class QuotesList(generics.ListCreateAPIView):
     queryset = CryptQuotes.objects.all()
     serializer_class = PriceSerializer
     permission_classes = (permissions.AllowAny,)
+
+
+class QuotesNow(APIView):
+    def get(self, request):
+        url_param = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+        params = {'X-CMC_PRO_API_KEY': 'd61bca4c-e9d3-40b9-8d82-abf9b057ffbd', "id": 1}
+        response = requests.get(url_param, params).json()
+        return Response(response)
